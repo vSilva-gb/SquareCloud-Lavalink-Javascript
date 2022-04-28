c802e639e60841334b0d47f5035c524533f1c6f6
vSilva-gb <abnersilva31@hotmail.com>
2022-04-28 03:03:09 -0300

    Fix

---
+++ bot/index.js
@@ -0,0 +1,23 @@
+//Create a discord bot
+const {Client, Intents} = require('discord.js');
+//Create a client with intents
+const client = new Client({
+    //Intents
+    intents: [Intents.FLAGS.GUILDS]
+});
+
+//Crie o evento ready
+client.on('ready', () => {
+    console.log('I am ready!');
+    //Crie uma função para usar o status viewing
+    client.user.setActivity('O meu dono me criar novamente... Em breve!', {type: 'WATCHING'});
+});
+
+//Crie o evento message
+client.on('message', message => {
+    if (message.content.includes['!']) {
+        console.log(message.content)
+    }
+});
+
+client.login('NjYxNDEwMjk4ODY2OTU4MzM5.XgrAVw.B3diC3Cv-i8JZay7Vig8NzRbZTs');
